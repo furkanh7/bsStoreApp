2fb9f745102605f6c0b9fa48c4f8283bf661f9cc
furkanh7 <furkanhilaloglu9@gmail.com>
2025-03-14 19:06:56 +0300

    Add project files.

---
+++ WebApi/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class Book
+
+    {
+
+        public int Id{ get; set; }
+        public String Title { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+using WebApi.Repositories.Config;
+
+namespace WebApi.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Price)
+                .HasPrecision(18, 4); // 18 basamak, 4 ondalık basamak
+
+            modelBuilder.ApplyConfiguration(new BookConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+    }
+}
